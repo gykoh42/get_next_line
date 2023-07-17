1a4678a5b9057e72902f1618b79f36ea1addf235
Gyeonga Koh <gykoh@cx1r2s4.42seoul.kr>
2023-07-17 15:15:38 +0900

    [error] fix segfault

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gykoh <gykoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 15:00:26 by gykoh             #+#    #+#             */
-/*   Updated: 2023/07/16 18:04:42 by gykoh            ###   ########.fr       */
+/*   Updated: 2023/07/17 15:14:47 by gykoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ static char *ft_read_line(int fd, char *storage)
     char buff[BUFFER_SIZE + 1];
     char *tmp;
 
-    while (1)
+    read_size = 1;
+    while (read_size > 0)
     {
         read_size = read(fd, buff, BUFFER_SIZE);
         if (read_size < 0)
@@ -26,15 +27,16 @@ static char *ft_read_line(int fd, char *storage)
             free(storage);
             return (NULL);
         }
-        if (read_size == 0 || read_size < BUFFER_SIZE) // 버퍼 오버플로우 수정
-            break;
         buff[read_size] = '\0';
-        tmp = storage;
-        if (!tmp)
-            tmp = ft_strdup("");
-        storage = ft_strjoin(tmp, buff);
-        free(tmp);
-        if (ft_strchr(storage, '\n'))
+        if (!storage)
+            storage = ft_strdup(buff);
+        else
+        {
+            tmp = ft_strjoin(storage, buff);
+            free(storage);
+            storage = tmp;
+        }
+        if (ft_strchr(buff, '\n'))
             break;
     }
     return (storage);
@@ -42,7 +44,7 @@ static char *ft_read_line(int fd, char *storage)
 
 static char *ft_extract_line(char **storage)
 {
-    static char *line;
+    char *line;
     char *newline_pos;
     size_t line_len;
 
@@ -51,7 +53,7 @@ static char *ft_extract_line(char **storage)
     {
         line_len = newline_pos - *storage;
         line = (char *)malloc(sizeof(char) * (line_len + 1));
-        ft_strlcpy(line, *storage, line_len + 1); // 정의되지 않은 동작 수정
+        ft_strlcpy(line, *storage, line_len + 1);
         line[line_len] = '\0';
         *storage = ft_strdup(newline_pos + 1);
     }
@@ -59,7 +61,7 @@ static char *ft_extract_line(char **storage)
     {
         line_len = ft_strlen(*storage);
         line = (char *)malloc(sizeof(char) * (line_len + 1));
-        ft_strlcpy(line, *storage, line_len + 1); // 정의되지 않은 동작 수정
+        ft_strlcpy(line, *storage, line_len + 1);
         line[line_len] = '\0';
         free(*storage);
         *storage = NULL;
@@ -77,78 +79,7 @@ char *get_next_line(int fd)
     storage = ft_read_line(fd, storage);
     if (!storage)
         return (NULL);
-    free(line); // 메모리 누수 수정
+    free(line);
     line = ft_extract_line(&storage);
     return (line);
 }
-
-// #include "get_next_line.h"
-
-// static char *ft_read_line(int fd, char *storage)
-// {
-// 	size_t  read_size;
-// 	char    buff[BUFFER_SIZE + 1];
-// 	char    *tmp;
-
-// 	while (1)
-// 	{
-// 		read_size = read(fd, buff, BUFFER_SIZE);
-// 		if (read_size < 0)
-// 		{
-// 			free(storage);
-// 			return (NULL);
-// 		}
-// 		if (read_size == 0)
-// 			break ;
-// 		buff[read_size] = '\0';
-// 		tmp = storage;
-// 		if (!tmp)
-// 			tmp = ft_strdup("");
-// 		storage = ft_strjoin(tmp, buff);
-// 		free(tmp);
-// 		if (ft_strchr(storage, '\n'))
-// 			break ;
-// 	}
-// 	return (storage);
-// }
-
-// static char *ft_extract_line(char **storage)
-// {
-//     static char *line;
-//     char *newline_pos;
-//     size_t line_len;
-
-//     newline_pos = ft_strchr(*storage, '\n');
-//     if (newline_pos)
-//     {
-//         line_len = newline_pos - *storage;
-//         line = (char *)malloc(sizeof(char) * (line_len + 1));
-//         ft_strncpy(line, *storage, line_len);
-//         line[line_len] = '\0';
-//         *storage = ft_strdup(newline_pos + 1);
-//     }
-//     else
-//     {
-//         line_len = ft_strlen(*storage);
-//         line = (char *)malloc(sizeof(char) * (line_len + 1));
-//         ft_strncpy(line, *storage, line_len);
-//         line[line_len] = '\0';
-//         free(*storage);
-//         *storage = NULL;
-//     }
-//     return (line);
-// }
-
-// char	*get_next_line(int fd)
-// {
-// 	static char	*line;
-// 	char	*storage = NULL;
-
-// 	if (fd < 0 || BUFFER_SIZE <= 0)
-// 		return (NULL);
-// 	storage = ft_read_line(fd, storage);
-// 	if (!storage)
-// 		return (NULL);
-// 	line = ft_extract_line(&storage);
-// 	return (line);
-// }
